cf0fe6590f261d8f3a321c89800481117c26e15a
ten <tenybo@163.com>
2018-09-17 18:13:58 +0800

    git 笔记

--- git使用笔记/git1.py
+++ git使用笔记/git1.py
@@ -3,5 +3,32 @@
 
 '''
 在github上面建立仓库
-，
+    your repositories
+    NEW
+
+
+    Repository name
+    Description
+
+    public  免费
+    private 收费
+
+
+在pycharm上连接github
+进入设置 version crontrl中配置github的各个选项
+    host    ->Token/Password
+        password:
+            Login:邮箱
+            Password:密码
+    点击测试是否连接成功
+        因为我的pycharm添加了Proxy导致连接失败：
+        进入设置去掉proxy设置即可
+
+
+github提交问题：
+    直接提交会失败
+    需要先建立分支
+    之后提交就可以成功了
+
+
 '''
