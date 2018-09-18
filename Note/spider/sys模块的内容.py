1c7d781bf818a01328e8106ad3468470b4e7a44b
ten <tenybo@163.com>
2018-09-18 09:55:32 +0800

    sys的信息

---
+++ Note/spider/sys模块的内容.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*
+# Time    : 2018/9/18 9:55
+import sys
+
+print(sys.argv)
+# python 8_24_sys.py 11 23 34 222 456
+# ['8_24_sys.py', '11', '23', '34', '222', '456']
+
+
+"""
+['__displayhook__', '__doc__', '__excepthook__', '__name__', '__package__', '__stderr__', 
+'__stdin__', '__stdout__', '_clear_type_cache', '_current_frames', '_getframe', '_git', 
+'api_version', 
+'argv'  传递给Python脚本的命令行参数列表。argv[0]是脚本的名称, 
+'builtin_module_names', 'byteorder', 'call_tracing', 'callstats', 
+'copyright', 'displayhook', 'dllhandle', 'dont_write_bytecode', 'exc_clear', 'exc_info', 
+'exc_type', 'excepthook', 'exec_prefix', 'executable', 'exit', 'flags', 'float_info', 
+'float_repr_style', 'getcheckinterval', 'getdefaultencoding', 'getfilesystemencoding', 
+'getprofile', 'getrecursionlimit', 'getrefcount', 'getsizeof', 'gettrace', 'getwindowsversion', 
+'hexversion', 'long_info', 'maxint', 'maxsize', 'maxunicode', 'meta_path', 'modules', 'path', 
+'path_hooks', 'path_importer_cache', 'platform', 'prefix', 'py3kwarning', 'setcheckinterval', 
+'setprofile', 'setrecursionlimit', 'settrace', 'stderr', 'stdin', 'stdout', 'subversion', 
+'version', 'version_info', 'warnoptions', 'winver']
+"""
+
+
+"""
+/en/ais/details/ships/shipid:5056180/mmsi:538004299/vessel:538004299
+https://www.marinetraffic.com/en/ais/details/ships/shipid:5056180/mmsi:538004299/vessel:538004299
+/en/ais/details/ships/shipid:713499/mmsi:538004299/vessel:538004299
+https://www.marinetraffic.com/en/ais/details/ships/shipid:713499/mmsi:538004299/vessel:538004299
+"""
